b129f367693d263802542c8f6014e2ccbaec8c54
jameem-pixel <jameem95@gmail.com>
2021-10-23 10:22:45 -0700

    port

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6fw9og1maxb-h%##1h+8pw81vc@qtv)9v%@wwl^#+knqk+wuh2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
